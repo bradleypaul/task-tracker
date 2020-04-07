1a05682af6fe2043d74a0b4fed9532c32b7aa087
Paul Bradley <bradleypauld@gmail.com>
2020-04-07 14:38:29 -0500

    update form functionality

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,6 +1,24 @@
-document.querySelector('.btn').addEventListener("click", function() {
-    const element = document.createElement('li');
-    element.classList.add('task-item');
-    element.innerText = "Fart";
-    document.querySelector('.task-list').append(element);
-})
+const tasksToDoEl = document.querySelector('#tasks-to-do');
+const formEl = document.querySelector('#task-form');
+const createTaskHandler = function (event) {
+    event.preventDefault();
+    const taskNameInput = document.querySelector("input[name='task-name']").value;
+    const taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener('submit', createTaskHandler);
